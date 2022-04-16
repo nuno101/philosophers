08de18fe2623ef34b02e9baf6378b031202c399a
Nuno <nlouro@student.42heilbronn.de>
2022-04-16 16:04:07 +0100

    Parse user input

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 12:34:25 by nlouro            #+#    #+#             */
-/*   Updated: 2022/03/24 17:08:56 by nlouro           ###   ########.fr       */
+/*   Updated: 2022/04/16 15:37:36 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,27 @@
 
 int	main(int argc, char **argv)
 {
-	printf("Philo\n");
-	printf("argc: %d\n", argc);
-	if (argc > 1)
-		printf("argv[1]: %s", argv[1]);
+	int	number_of_philosophers;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int number_of_times_each_philosopher_must_eat;
+
+	if (argc < 5)
+		printf("Error: missing arguments\n");
+	else
+	{
+		printf("number_of_philosophers: %s\n", argv[1]);
+		printf("time_to_die: %s\n", argv[2]);
+		printf("time_to_eat: %s\n", argv[3]);
+		printf("time_to_sleep: %s\n", argv[4]);
+		number_of_philosophers = ft_atoi(argv[1]);
+		time_to_die = ft_atoi(argv[2]);
+		time_to_eat = ft_atoi(argv[3]);
+		time_to_sleep = ft_atoi(argv[4]);
+	}
+	if (argc > 5)
+		printf("number_of_times_each_philosopher_must_eat (opt): %s\n", argv[5]);
+		number_of_times_each_philosopher_must_eat = ft_atoi(argv[5]);
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 12:34:49 by nlouro            #+#    #+#             */
-/*   Updated: 2022/03/24 17:03:25 by nlouro           ###   ########.fr       */
+/*   Updated: 2022/04/16 15:45:45 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 
 #include <unistd.h>
 #include <stdio.h> // used for printf
+#include <pthread.h> // used for mutex functions 
+#include "ft_atoi.c"
 
 #endif
